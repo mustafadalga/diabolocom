edbef296675f5f063761b39d48eb311a34daa54e
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-05 22:47:22 +0300

    Unit Test: Add unit tests for displaying count from store and rendering correct label based on locale

---
+++ src/components/MetricsDisplay.test.ts
@@ -0,0 +1,35 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { VueWrapper } from "@vue/test-utils";
+import MetricsDisplay from "./MetricsDisplay.vue";
+import store from "@/store";
+import { i18n, messages } from "@/utilities/i18n.ts";
+import mountComponent from "@/utilities/mountComponent.ts";
+
+const defaultLocale = i18n.global.locale.value;
+
+describe('MetricsDisplay', () => {
+    let wrapper: VueWrapper<typeof MetricsDisplay>;
+
+    beforeEach(() => {
+        store.mutations.reset();
+        i18n.global.locale.value = defaultLocale
+        wrapper = mountComponent<typeof MetricsDisplay>(MetricsDisplay)
+    });
+
+    it('should display the correct count from the store', async () => {
+        store.mutations.increment(); // Increment to make count 1
+
+        // Check if the count is displayed correctly
+        expect(wrapper.html()).toContain('<p>1</p>');
+    });
+
+    it('should display the correct label', async () => {
+        // Dynamically change the locale to French for this test
+        i18n.global.locale.value = 'fr';
+
+        expect(wrapper.text()).toContain(messages.fr.metricsLabel);
+    });
+})
+
+
+
