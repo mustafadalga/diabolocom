2c19d62f5f618b502109d2c05ee62b7831aea5ea
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-05 23:14:55 +0300

    UI: Reduced max width of buttons

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -7,7 +7,7 @@ const { t } = useI18n()
 
 <template>
   <button
-      class="w-full max-w-40 text-xs lg:text-sm text-white bg-purple-600 hover:bg-purple-700 font-medium rounded-lg px-5 py-2.5"
+      class="w-full max-w-36 text-xs lg:text-sm text-white bg-purple-600 hover:bg-purple-700 font-medium rounded-lg px-5 py-2.5"
       data-testid="btn-reset"
       @click="store.mutations.reset()">
     {{ t("reset") }}
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -10,14 +10,14 @@ const { t } = useI18n()
   <div class="grid place-items-center gap-5">
     <div class="w-full flex justify-center items-center gap-5">
       <button
-          class="w-full max-w-60	text-xs lg:text-sm text-white bg-purple-600 hover:bg-purple-700 font-medium rounded-lg px-5 py-2.5"
+          class="w-full max-w-48	text-xs lg:text-sm text-white bg-purple-600 hover:bg-purple-700 font-medium rounded-lg px-5 py-2.5"
           @click="store.mutations.increment()"
           data-testid="btn-increment">
         {{ t("increment") }}
       </button>
 
       <button
-          class="w-full max-w-60 text-xs lg:text-sm text-white bg-purple-600 hover:bg-purple-700 font-medium rounded-lg px-5 py-2.5"
+          class="w-full max-w-48 text-xs lg:text-sm text-white bg-purple-600 hover:bg-purple-700 font-medium rounded-lg px-5 py-2.5"
           @click="store.mutations.decrement()"
           data-testid="btn-decrement">
         {{ t("decrement") }}
