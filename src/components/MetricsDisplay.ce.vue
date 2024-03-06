c9720e7fcf1a04577ef1692cfa941a0e405eda01
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-06 23:14:33 +0300

    UI: Add border

--- src/components/MetricsDisplay.ce.vue
+++ src/components/MetricsDisplay.ce.vue
@@ -9,7 +9,7 @@ const count = computed<number>(() => store.state.count);
 
 <template>
   <div
-      class="w-full max-w-2xl mx-auto grid items-center justify-center gap-10 p-8 shadow-[0_2px_3px_rgba(0,0,0,0.06)] bg-white rounded-lg">
+      class="w-full max-w-2xl mx-auto grid items-center justify-center gap-10 p-8 shadow-[0_2px_3px_rgba(0,0,0,0.06)] bg-white border border-solid border-gray-200 rounded-lg">
     <div class="grid gap-3 text-purple-500 text-xl lg:text-2xl text-center">
       <h3>{{ t('metricsLabel') }}</h3>
       <p>{{ count }}</p>
