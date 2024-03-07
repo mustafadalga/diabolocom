ff06c164664b4e60e135455e6a361f69167d697b
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-07 11:25:56 +0300

    Demo: Prepare demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from "node:url";
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [ vue({
-        template: {
-            compilerOptions: {
-                isCustomElement: tag => tag.includes('-')
-            }
-        }
-    }), ],
+    plugins: [ vue(), ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-    build: {
-        outDir: 'dist/elements',
-        rollupOptions: {
-            input: {
-                'i18n-host': resolve(__dirname, 'src/elements/i18n-host.ts'),
-                'metrics-display': resolve(__dirname, 'src/elements/metrics-display.ts'),
-                'counter': resolve(__dirname, 'src/elements/counter.ts'),
-                'locale-picker': resolve(__dirname, 'src/elements/locale-picker.ts'),
-                'control-panel': resolve(__dirname, 'src/elements/control-panel.ts'),
-            },
-            output: {
-                entryFileNames(chunkInfo) {
-                    return `${chunkInfo.name}.js`
-                },
-            },
-        }
-    }
-
 })
